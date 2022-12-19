f124f00d4cbfc8bc1beaa32031a730c789a2f4e8
albcl <a.cerveto@gmail.com>
2022-12-20 00:15:14 +0100

    Use Match() for game details

--- src/Board/index.ts
+++ src/Board/index.ts
@@ -1,7 +1,8 @@
+import Match from '../Match';
 import { isString, isValidArray, isvalidScore, setKey } from '../utils';
 
 interface Board {
-    _matches: Map<string, number[]>;
+    _matches: Map<string, Match>;
 }
 
 class Board {
@@ -38,7 +39,7 @@ class Board {
                 throw new Error('Teams need to be string');
             }
             this._isTeamPlaying(teams);
-            this._matches.set(setKey(teams), [0, 0]);
+            this._matches.set(setKey(teams), new Match(teams));
         } catch (error) {
             throw new Error(error);
         }
@@ -69,7 +70,8 @@ class Board {
             throw new Error(`There is not ${teams.join(' - ')} match`);
         }
 
-        this._matches.set(mapKey, score);
+        const match = this._matches.get(mapKey);
+        match.updateScore(score);
     }
 
     /**
@@ -95,18 +97,17 @@ class Board {
      * @returns {string[]} Live matches
      */
     getSummary() {
-        return [...this._matches]
+        return [...this._matches.values()]
             .sort((matchA, matchB) => {
-                const totalScoreA = matchA[1][0] + matchA[1][1];
-                const totalScoreB = matchB[1][0] + matchB[1][1];
+                const totalScoreA = matchA.score[0] + matchA.score[1];
+                const totalScoreB = matchB.score[0] + matchB.score[1];
 
                 if (totalScoreA > totalScoreB) return -1;
                 if (totalScoreA < totalScoreB) return 1;
                 return 0;
             })
             .map(match => {
-                const teams = match[0];
-                const score = match[1];
+                const { teams, score } = match;
                 return `${teams[0]} ${score[0]} - ${teams[1]} ${score[1]}`;
             });
     }

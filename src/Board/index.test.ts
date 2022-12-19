b5f41ac4462992ab5ee104a2478c005ec046c25a
albcl <a.cerveto@gmail.com>
2022-12-19 10:16:08 +0100

    Will add a match

--- src/Board/index.test.ts
+++ src/Board/index.test.ts
@@ -1,7 +1,18 @@
 import Board from '.';
 
 describe('Board Cases', () => {
+    let board: Board;
+
     test('Create a new board just fine', () => {
-        expect(() => new Board()).not.toThrowError();
+        expect(() => (board = new Board())).not.toThrowError();
+    });
+
+    test('Add match', () => {
+        const team = ['Team 01', 'Team 02'];
+        const expectedMatches = new Map();
+        expectedMatches.set(team, [0, 0]);
+
+        board.addMatch(team);
+        expect(board._matches).toMatchObject(expectedMatches);
     });
 });

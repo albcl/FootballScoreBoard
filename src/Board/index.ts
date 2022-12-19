466ef2e479a7e3291c9f7e0a5234381a72271f3a
albcl <a.cerveto@gmail.com>
2022-12-19 10:47:33 +0100

    Fail if team is already playing

--- src/Board/index.ts
+++ src/Board/index.ts
@@ -10,18 +10,38 @@ class Board {
     }
 
     /**
+     * Check if a team is already playing
+     *
+     * @param {string[]} teams
+     */
+    _isTeamPlaying(teams: string[]) {
+        teams.forEach(team => {
+            for (let [key, value] of this._matches) {
+                if (key.includes(team)) {
+                    throw new Error(`Team ${team} is already playing`);
+                }
+            }
+        });
+    }
+
+    /**
      * Start a new match
      *
      * @param {string[]} teams
      */
     addMatch(teams: string[]) {
-        if (!isValidArray(teams)) {
-            throw new Error('Teams need to be passed as an array of 2 elements');
+        try {
+            if (!isValidArray(teams)) {
+                throw new Error('`Teams` needs to be passed as an array of 2 elements');
+            }
+            if (!isString(teams)) {
+                throw new Error('Teams need to be string');
+            }
+            this._isTeamPlaying(teams);
+            this._matches.set(teams, [0, 0]);
+        } catch (error) {
+            throw new Error(error);
         }
-        if (!isString(teams)) {
-            throw new Error('Teams need to be string');
-        }
-        this._matches.set(teams, [0, 0]);
     }
 }
 

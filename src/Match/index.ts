ce1aa903068e7e1c785e7f1636ec2b94a5b7d415
albcl <a.cerveto@gmail.com>
2022-12-19 13:45:35 +0100

    Set a new match with `teams` and `score` properties

--- src/Match/index.ts
+++ src/Match/index.ts
@@ -1,5 +1,13 @@
+interface Match {
+    teams: string[];
+    score: number[];
+}
+
 class Match {
-    constructor() {}
+    constructor(teams: string[]) {
+        this.teams = teams;
+        this.score = [0, 0];
+    }
 }
 
 export default Match;

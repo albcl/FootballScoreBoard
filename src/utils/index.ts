5951d4e1554ee8cb027fdbcaf7df1f6a30182896
albcl <a.cerveto@gmail.com>
2022-12-19 11:15:15 +0100

    Util will validate score

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -23,3 +23,13 @@ export const isString = (values: any | any[]) => {
 export const isValidArray = (parameter: (string | number)[]) => {
     return typeof parameter === 'object' && parameter.length === 2;
 };
+
+/**
+ * Check if score is an array of two integer values
+ *
+ * @param {number[]} score
+ * @returns {boolean}
+ */
+export const isvalidScore = (score: number[]) => {
+    return 'to-do';
+};
